e1c35fd2bced71fb8337c2990d5a71f8b2b57ac7
149004517 <55717398+149004517@users.noreply.github.com>
2021-04-20 20:48:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function displayRadioValue() {
+            var ele = document.getElementsByName('meat');
+              
+            for(i = 0; i < ele.length; i++) {
+                if(ele[i].checked)
+                document.getElementById("result").innerHTML
+                        = "meat: "+ele[i].value;
+            }
+        }
