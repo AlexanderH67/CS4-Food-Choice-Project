d3900916fcfadfafe2870ff6ca4bf0d5b1be0061
AlexanderH67 <82112649+AlexanderH67@users.noreply.github.com>
2021-04-22 09:45:43 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
-function displayRadioValue() {
-            var ele = document.getElementsByName('meat');
-              
-            for(i = 0; i < ele.length; i++) {
-                if(ele[i].checked)
-                document.getElementById("result").innerHTML
-                        = "meat: "+ele[i].value;
-            }
-        }
+let express = require('express');
+var bodyParser = require('body-parser');
+//var routes = require("./routes");   //new
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', express.static('./'));   //new
+app.use('/js', express.static('./public/js'));   //new
+//app.use(routes);  //new
+
+
+//app.use('/', express.static('./'));   //new
+
+//req is info sending to server from client.
+//res is info sending to client from server.
+
+let port = process.env.PORT || 3000;
+app.listen(port);
