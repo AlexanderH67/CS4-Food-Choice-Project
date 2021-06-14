256d319dfb7836295e578da36bdb677e16c05efd
AlexanderH67 <82112649+AlexanderH67@users.noreply.github.com>
2021-06-13 22:28:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
-const http = require('http')
-const fs = require('fs')
-const port = 4011
 
-const server = http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type' : 'text/html'})
-	fs.readFile('FoodProject.html', function(error,data){
-		if(error){
-          res.writeHead(404)
-          res.write('Error: File Not Found')
-		} else {
-			res.write(data)
-		}
-		res.end()
-	})
-
-})
-
-server.listen(port,function(error){
-  if(error){
-  	console.log('Something Went Wrong')
-  } else {
-    console.log('Server is listening on port' + port)
-  }
-})
-
-/*
 let express = require('express');
 var bodyParser = require('body-parser');
-//var routes = require("./routes");   //new
+var routes = require("./routes");   //new
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static('./'));   //new
 app.use('/js', express.static('./public/js'));   //new
-//app.use(routes);  //new
+app.use(routes);  //new
 
 
 //app.use('/', express.static('./'));   //new
@@ -44,4 +18,4 @@ app.use('/js', express.static('./public/js'));   //new
 
 let port = process.env.PORT || 4011;
 app.listen(port);
-*/
+
